b460ed47d5c9a6a1d124045da6dd27c101119e6e
Louis <louis.mueller@uzh.ch>
2020-10-26 20:05:22 +0100

    init

---
+++ src/arguments.py
@@ -0,0 +1,10 @@
+# default arguments
+args = {
+    'annotation_file' : '../input/multiclass_aij.csv',
+    'labels_file' : '../input/multiclass_labels.csv',
+    'T' : 300,
+    'gamma' : 1,
+    'mu' : 2,
+    'alpha' : 2,
+    'beta' : 2
+}
---
+++ src/sampler.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import arguments
+import math
+from sklearn.metrics import accuracy_score
+
+LABEL_NAMES = ['emerging', 'established', 'no_option']
+NUMBER_OF_LABELS = len(LABEL_NAMES)
+LABEL_INDEX = np.array(range(0,NUMBER_OF_LABELS))
+
+def run(annotation_file,labels_file,T,gamma,mu,alpha,beta):
+    labels = pd.read_csv(labels_file, sep=",")
+    labels['label_code'] = pd.factorize(labels['label'],sort=True)[0] + 1
+
+    annotation_matrix = pd.read_csv(annotation_file, sep=",",header=None)
+    annotation_matrix['label_code'] = pd.factorize(annotation_matrix[2],sort=True)[0] + 1
+
+    assert annotation_matrix[1].unique().shape[0] == labels.shape[0]
+
+    n_workers = annotation_matrix[0].unique().shape[0]
+    
+    n_items = annotation_matrix[1].unique().shape[0]
+
+    z_i = np.random.randn(n_items ,1) * math.sqrt(1/gamma) + mu
+    while np.where( z_i < 0 )[0].shape[0] > 0:
+        z_i = np.random.randn(n_items ,1) * math.sqrt(1/gamma) + mu
+
+    r_j = np.random.gamma(alpha,beta,(n_workers,1))
+    while np.where( r_j < 0 )[0].shape[0] > 0:
+        r_j = np.random.gamma(alpha,beta,(n_workers,1))
+
+
+    for t in range(T):
+        ground_truth = labels['label_code'].values
+        accuracy = accuracy_score(ground_truth,z_i.round().reshape(1,-1)[0])
+        print('t:',t,'accuracy',accuracy)
+
+        # for each item label
+        for i in annotation_matrix[1].unique():
+            sum_r_j = r_j[annotation_matrix[annotation_matrix[1] == i][0].values]
+            aij = annotation_matrix[annotation_matrix[1] == i]['label_code'].values
+            aij = aij.reshape((-1,1))
+            temp_gamma = sum_r_j.sum() + gamma
+            temp_mu = ((aij*sum_r_j).sum() + gamma*mu)/temp_gamma
+            z_i[i] = np.random.randn(1 ,1) * math.sqrt(1/temp_gamma) + temp_mu
+
+        # for each worker reliability
+        for j in annotation_matrix[0].unique():
+            sum_aij_zi = 0
+            for index, row in annotation_matrix[annotation_matrix[0] == j].iterrows():
+                # row[1] is the item id
+                sum_aij_zi += (row['label_code'] - z_i[row[1]])**2
+
+            temp_alpha = alpha + annotation_matrix[annotation_matrix[0] == j].shape[0] / 2
+            temp_beta = beta + 0.5 * sum_aij_zi
+            r_j[j] = np.random.gamma(temp_alpha,temp_beta,1)
+            
+
+        # tbd convergence break needed?
+
+if __name__ == '__main__':
+    # load default arguments
+    args = arguments.args
+
+    run(**args)
