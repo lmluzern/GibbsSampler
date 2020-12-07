c54ed8fcba47552b97d063365123e411f91a1243
Louis <louis.mueller@uzh.ch>
2020-12-07 14:32:35 +0100

    added convergence tracing

--- src/sampler.py
+++ src/sampler.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 import time
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 def getItemDict(annotation_matrix):
     J = {}
@@ -61,12 +63,12 @@ def neg_sampling(aij,sampling_rate):
 def sample_z_i(gamma_0,mu_0,r_j,a_ij):
     precision = r_j.sum() + gamma_0
     mean = ((r_j*a_ij).sum() + gamma_0 * mu_0)/precision
-    return np.random.normal(mean,1/np.sqrt(precision))
+    return np.random.normal(mean,1/np.sqrt(precision)),1/np.sqrt(precision),mean
 
 def sample_r_j(A_0,B_0,z_i,a_ij):
     A_new = A_0 + a_ij.shape[0]/2.0
     B_new = B_0 + ((a_ij-z_i)**2).sum()/2
-    return np.random.gamma(A_new,(1/B_new))
+    return np.random.gamma(A_new,(1/B_new)),A_new,1/B_new
 
 def gibbs(param,e2t):
     annotation_matrix = pd.read_csv(param['annotation_file'],sep=",",header=None)
@@ -79,16 +81,32 @@ def gibbs(param,e2t):
     all_workers = np.unique(annotation_matrix[:,0])
     all_items = np.unique(annotation_matrix[:,1])
 
-    r  = np.full((all_workers.shape[0],1),param['init_r'],dtype=float)
-    z = np.full((all_items.shape[0],1),-1,dtype=float)
+    # init r with mean A_0/B_0
+    r = np.full((all_workers.shape[0],1),param['A_0']/param['B_0'],dtype=float)
+    # r = np.random.gamma(param['A_0'],(1/param['B_0']),all_workers.shape[0]).reshape(-1,1)
+
+    z = np.full((all_items.shape[0],1),0,dtype=float)
     trace = np.zeros((param['iters'],all_items.shape[0]))
 
-    annotation_matrix = neg_sampling(annotation_matrix,param['sampling_rate'])
+    trace_precision = np.full((all_items.shape[0],1),0,dtype=float)
+    trace_mean = np.full((all_items.shape[0],1),0,dtype=float)
+    trace_A = np.full((all_workers.shape[0],1),0,dtype=float)
+    trace_B = np.full((all_workers.shape[0],1),0,dtype=float)
+
+    trace_conv = np.full((param['iters'],6),0,dtype=float)
+
+    # annotation_matrix = neg_sampling(annotation_matrix,param['sampling_rate'])
 
     J,a_j = getItemDict(annotation_matrix)
     I,a_i = getWorkerDict(annotation_matrix)
 
     for it in range(param['iters']):
+        prev_trace_precision = trace_precision.copy()
+        prev_trace_mean = trace_mean.copy()
+        prev_trace_z = z.copy()
+        prev_trace_A = trace_A.copy()
+        prev_trace_B = trace_B.copy()
+        prev_trace_r = r.copy()
         # for each item label
         for i in all_items:
             # supervision
@@ -97,41 +115,52 @@ def gibbs(param,e2t):
                 continue
             r_j = r[J[i]]
             a_ij = a_j[i]
-            z[i] = sample_z_i(param['gamma_0'],param['mu_0'],r_j,a_ij)
-            while z[i] <= lower_bound or z[i] >= upper_bound:
-                z[i] = sample_z_i(param['gamma_0'],param['mu_0'],r_j,a_ij)
+            z[i],trace_precision[i],trace_mean[i] = sample_z_i(param['gamma_0'],param['mu_0'],r_j,a_ij)
+            # while z[i] <= lower_bound or z[i] >= upper_bound:
+            #     z[i] = sample_z_i(param['gamma_0'],param['mu_0'],r_j,a_ij)
         trace[it,:] = z.transpose()[0].copy()
-
+        
         # for each worker reliability
         for j in all_workers:
             z_i = z[I[j]]
             a_ij = a_i[j]
-            r[j] = sample_r_j(param['A_0'],param['B_0'],z_i,a_ij)
+            r[j],trace_A[j],trace_B[j] = sample_r_j(param['A_0'],param['B_0'],z_i,a_ij)
 
-    return trace
+        trace_conv[it] = LA.norm(trace_precision-prev_trace_precision), LA.norm(trace_mean-prev_trace_mean), LA.norm(trace_A-prev_trace_A), LA.norm(trace_B-prev_trace_B), LA.norm(z-prev_trace_z), LA.norm(r-prev_trace_r)
+    return trace,trace_conv
 
 def run(param,e2t):
     start_t = time.time()
-    trace = gibbs(param,e2t)
+    trace,trace_conv = gibbs(param,e2t)
     z_median = np.median(trace[int(param['iters']*param['burn_in_rate']):],axis=0)
-    return z_median
+
+    if __name__ == '__main__':
+        plt.plot(trace_conv[:,0],label='precision')
+        plt.plot(trace_conv[:,1],label='mean')
+        plt.plot(trace_conv[:,2],label='A_new')
+        plt.plot(trace_conv[:,3],label='B_new')
+        plt.plot(trace_conv[:,4],label='z')
+        plt.plot(trace_conv[:,5],label='r')
+        plt.legend(loc="upper right")
+        plt.show()
+
+    return z_median, trace_conv
 
 
 if __name__ == '__main__':
     param = {
         'annotation_file' : '../input/multiclass_aij.csv',
         'labels_file' : '../input/multiclass_labels.csv',
-        'A_0' : 8,
-        'B_0' : 6,
-        'gamma_0' : 1.25,
-        'mu_0' : 4.25,
-        'init_r' : 4,
-        'iters' : 10,
+        'A_0' : 2,
+        'B_0' : 2,
+        'gamma_0' : 0.5,
+        'mu_0' : 2,
+        'iters' : 20,
         'burn_in_rate' : 0.5,
         'supervision_rate' : 0.0,
         'sampling_rate' : 0.1
         }
-    z_median = run(param,{})
+    z_median, trace_conv = run(param,{})
     ground_truth = pd.factorize(pd.read_csv(param['labels_file'],sep=",")['label'],sort=True)[0] + 1
     accuracy = accuracy_score(ground_truth,z_median.round())
     print('accuracy',accuracy)
